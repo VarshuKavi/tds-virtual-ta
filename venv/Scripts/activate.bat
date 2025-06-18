61d1732e177a9c5269a85c286d80eafb1849d78e
Varshini M <varshukavi2003@gmail.com>
2025-06-18 23:29:45 +0530

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\IIT Madras\TDS\Project_00001\venv
+set VIRTUAL_ENV=E:\IIT Madras\TDS\22f1000156\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
